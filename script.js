bda0bcc2c65a7991b196d6d9a66a073083e61196
Barbara Toniella Corradini <102234758+bcorrad@users.noreply.github.com>
2024-04-03 11:58:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 function changeLanguage(lang) {
     var currentUrl = window.location.href;
     var baseUrl = currentUrl.split('?')[0]; // Rimuove eventuali parametri dalla URL corrente
-    var newUrl;
 
-    // Aggiunge il parametro lang alla URL in base alla lingua selezionata
-    if (lang === 'en') {
-        newUrl = baseUrl + '?lang=en';
-    } else if (lang === 'it') {
-        newUrl = baseUrl + '?lang=it';
+    // Estrae il valore del parametro lang dall'URL
+    var urlParams = new URLSearchParams(window.location.search);
+    var currentLang = urlParams.get('lang');
+
+    // Costruisce l'URL della nuova pagina in base alla lingua selezionata
+    var newUrl;
+    if (lang === 'it') {
+        newUrl = 'index_it.html';
+    } else {
+        newUrl = 'index.html';
     }
 
-    // Redirect alla nuova URL
-    window.location.href = newUrl;
+    // Reindirizza l'utente alla nuova URL solo se la lingua selezionata è diversa dalla lingua corrente
+    if (currentLang !== lang) {
+        window.location.href = newUrl;
+    }
 }
